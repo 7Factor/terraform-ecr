87a11f89ca69df0dc9924d301acc28b6f724bfde
jduv <jduv@7factor.io>
2019-01-21 19:48:06 -0500

    First commit

---
+++ ecr.tf
@@ -0,0 +1,74 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_ecr_repository" "dotnetcore_starter" {
+  name = "${var.repo_name}"
+}
+
+resource "aws_ecr_lifecycle_policy" "keep_most_recent_policy" {
+  repository = "${aws_ecr_repository.dotnetcore_starter.name}"
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last ${var.images_to_keep} images with `any` tag",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": ${var.images_to_keep}
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_repository_policy" "repository_policy" {
+  repository = "${aws_ecr_repository.dotnetcore_starter.name}"
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowCrossAccountPull",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::${var.dev_account_id}:root"
+            },
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability"
+            ]
+        },
+        {
+            "Sid": "AllowCrossAccountPull",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::${var.prod_account_id}:root"
+            },
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability"
+            ]
+        },
+        {
+            "Sid": "AllowAll",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+            },
+            "Action": [
+                "ecr:*"
+            ]
+        }
+    ]
+}
+EOF
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "dev_account_id" {
+  description = "Account ID for the development account. Allows cross account pulls."
+}
+
+variable "prod_account_id" {
+  description = "Account ID for the production account. Allows cross account pulls."
+}
+
+variable "repo_name" {
+  description = "The name of the repository to create."
+}
+
+variable "images_to_keep" {
+  default = 10
+  description = "Then number of images to keep in the repo."
+}
