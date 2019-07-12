902726ded9adde0f8254c895ef08f22e1fad5b6d
Richard Dohn <richard.dohn@7factor.io>
2019-07-12 10:50:36 -0400

    support tf 0.12.3

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ecr_repository_urls" {
-  value       = "${aws_ecr_repository.repos.*.repository_url}"
+  value       = aws_ecr_repository.repos.*.repository_url
   description = "A list of urls of the repositories that were created."
 }
