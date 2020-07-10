e308a89031ab59db6755d84afb8bd77e2963be28
chris-malloy <chrismalloymusic@gmail.com>
2020-07-10 11:00:20 -0400

    Simplifying logic.

--- integration/spec/main_spec.rb
+++ integration/spec/main_spec.rb
@@ -2,16 +2,20 @@ require 'awspec'
 require 'hcl/checker'
 require 'json'
 
-require_relative 'util'
-
 TFVARS = HCL::Checker.parse(File.open('testing.tfvars').read())
 
 describe ecr_repository(TFVARS['repository_list'][0]) do
   it { should exist }
-  test_policy_text('stub/repo-1-policy.json')
+
+  context 'has the correct policy text' do
+    its(:policy_text) { should eq File.read('stub/repo-1-policy.json') }
+  end
 end
 
 describe ecr_repository(TFVARS['repository_list'][1]) do
   it { should exist }
-  test_policy_text('stub/repo-2-policy.json')
+
+  context 'has the correct policy text' do
+    its(:policy_text) { should eq File.read('stub/repo-2-policy.json') }
+  end
 end
